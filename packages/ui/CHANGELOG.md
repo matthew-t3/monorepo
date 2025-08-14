f5737dcd20d42e57a5a98ce767c373d46eb9ce93
Matthew Torres <157339328+matthew-t3@users.noreply.github.com>
2025-08-14 12:50:24 +0800

    feat: add variants to button component (#10)

--- packages/ui/CHANGELOG.md
+++ packages/ui/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @repo/ui
 
+## 1.2.0
+
+### Minor Changes
+
+- [#10](https://github.com/matthew-t3/monorepo/pull/10) [`1211ce6`](https://github.com/matthew-t3/monorepo/commit/1211ce6afbc22056b3d68ce77395ca4ee8aec77e) Thanks [@matthew-t3](https://github.com/matthew-t3)! - add variants to button
+
 ## 1.1.0
 
 ### Minor Changes
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -6,12 +6,18 @@ interface ButtonProps {
   children: ReactNode;
   className?: string;
   appName: string;
+  variant?: "primary" | "secondary";
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({
+  children,
+  className,
+  appName,
+  variant = "primary",
+}: ButtonProps) => {
   return (
     <button
-      className={className}
+      className={className + " " + variant}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
